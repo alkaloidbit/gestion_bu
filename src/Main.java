cfd31a5170b62403ee0c92d74e9124104b3417d8
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-06-11 02:51:38 +0200

    Clean Main class

--- src/Main.java
+++ src/Main.java
@@ -21,40 +21,6 @@ public class Main {
 
 //        db.createOrUpdateOrDelete("INSERT INTO `genre` (`id_edition`, `name`) VALUES (NULL, \"The expert's voice in open source\")");
 
-
-
         db.disconnect();
-//        try (ResultSet user_list = stmt.executeQuery("SELECT * FROM  gestion_bu.`user`")) {
-//                   while (user_list.next()) {
-//                       System.out.println(String.format("%s %s <%s>",
-//                               user_list.getString("first_name"),
-//                               user_list.getString("last_name"),
-//                               user_list.getString("email")));
-//                   }
-//       }
-        // Try to connect
-//        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/gestion_bu", "root", "RvTC6F8D")) {
-//            try(Statement stmt = connection.createStatement()) {
-//               try (ResultSet user_list = stmt.executeQuery("SELECT * FROM  gestion_bu.`user`")) {
-//                   while (user_list.next()) {
-//                       System.out.println(String.format("%s %s <%s>",
-//                               user_list.getString("first_name"),
-//                               user_list.getString("last_name"),
-//                               user_list.getString("email")));
-//                   }
-//               }
-//
-//               try (ResultSet doc_list = stmt.executeQuery("select d.title, d.pages_nbr, d.year, e.name as edition from document d inner join edition e on e.id_edition = d.id_edition;")) {
-//                    while (doc_list.next()) {
-//                        System.out.println(String.format("%s %s %s <%s>",
-//                                doc_list.getString("title"),
-//                                doc_list.getString("pages_nbr"),
-//                                doc_list.getString("year"),
-//                                doc_list.getString("edition")));
-//                    }
-//                }
-//            }
-//            connection.close();
-//        }
     }
 }
