9deb57d4a113df2348849ae0b0767d0a15c1780f
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-06-11 22:30:51 +0200

    feat: First Swing test

---
+++ src/TestSwing.java
@@ -0,0 +1,28 @@
+
+import javax.swing.*;
+import java.awt.event.*;
+
+
+
+public class TestSwing extends JFrame {
+
+    public TestSwing() {
+        super("Universary  Library");
+
+        WindowListener  l = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+
+        addWindowListener(l);
+        setSize(1500,750);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame freme = new TestSwing();
+    }
+
+}
