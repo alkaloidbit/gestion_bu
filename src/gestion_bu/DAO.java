4f2d3622b126d27232a6260e22f9ac7fb4a9fa1e
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-06-15 23:11:00 +0200

    feat: Add DAO abstract class

--- src/gestion_bu/DAO.java
+++ src/gestion_bu/DAO.java
@@ -1,8 +1,16 @@
 package gestion_bu;
 
 
+import java.sql.Connection;
+
 public abstract  class DAO<T> {
+    public Connection connect = ConnectionMariaDB.getInstance();
 
+    public abstract T find(int id);
 
+    public abstract T create(T obj);
 
+    public abstract T update(T obj);
+
+    public abstract void delete(T obj);
 }
